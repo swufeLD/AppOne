dd67b4636e9d2e51554b55955e81eee0b5961276
swufeLD <LD666LY@163.com>
2020-09-15 23:24:54 +0800

    second submit

--- app/src/main/java/com/example/appone/MainActivity.java
+++ app/src/main/java/com/example/appone/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity  {
     @Override
+    //second submit
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
